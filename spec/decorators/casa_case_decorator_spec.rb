d2dcb9fae6ba5eadbef195d2b54cbd02484e75b2
CW <compiledwrong+github@gmail.com>
2022-01-18 21:22:15 -0800

    Fix some Yes vs true from decorator, oops

--- spec/decorators/casa_case_decorator_spec.rb
+++ spec/decorators/casa_case_decorator_spec.rb
@@ -55,25 +55,25 @@ RSpec.describe CasaCaseDecorator do
   describe "#has_transitioned?" do
     it "returns transition age youth status with icon if transition age youth && birthday is nil" do
       casa_case = build(:casa_case, :transition_aged, birth_month_year_youth: nil)
-      expect(casa_case.decorate.has_transitioned?)
+      expect(casa_case.decorate.transition_aged_youth)
         .to eq "Yes #{CasaCase::TRANSITION_AGE_YOUTH_ICON}"
     end
 
     it "returns transition age youth status with icon if not transition age youth && birthday is nil" do
       casa_case = build(:casa_case, :not_transition_aged, birth_month_year_youth: nil)
-      expect(casa_case.decorate.has_transitioned?)
+      expect(casa_case.decorate.transition_aged_youth)
         .to eq "No #{CasaCase::NON_TRANSITION_AGE_YOUTH_ICON}"
     end
 
     it "returns transition age youth status with icon if over 14 years old" do
       casa_case = build(:casa_case, birth_month_year_youth: 14.years.ago)
-      expect(casa_case.decorate.has_transitioned?)
+      expect(casa_case.decorate.transition_aged_youth)
         .to eq "Yes #{CasaCase::TRANSITION_AGE_YOUTH_ICON}"
     end
 
     it "returns non-transition age youth status with icon if not over 14 years old" do
       casa_case = build(:casa_case, birth_month_year_youth: 13.years.ago)
-      expect(casa_case.decorate.has_transitioned?)
+      expect(casa_case.decorate.transition_aged_youth)
         .to eq "No #{CasaCase::NON_TRANSITION_AGE_YOUTH_ICON}"
     end
   end
